8f5ca0493961775fc42aba1413b4763915e4dee7
jose5556 <josefilmeneses@gmail.com>
2024-05-13 06:30:41 +0100

    main ready to test everything

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 03:56:32 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/05/13 06:19:57 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/05/13 06:23:50 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ char *get_next_line(int fd)
 {
 	char	*buffer;
 
+	
 	while (read(fd, buffer, 1))
 	{
 		printf("%s", buffer);
