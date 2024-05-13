6844ae09b811953cddc5828e859dcb12477daa1d
jose5556 <josefilmeneses@gmail.com>
2024-05-13 22:02:30 +0100

    attemped to add buffer_size

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 03:56:32 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/05/13 06:23:50 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/05/13 21:56:49 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,22 @@ char *get_next_line(int fd);
 char *get_next_line(int fd)
 {
 	char	*buffer;
-
 	
+	buffer = calloc(1, sizeof(char));
 	while (read(fd, buffer, 1))
 	{
 		printf("%s", buffer);
 	}
+	free(buffer);
 	return (NULL);
 }
+
+
 
 int	main(void)
 {
 	int	fd;
 
-	fd = open("./example.txt", O_RDONLY, O_RDONLY);
+	fd = open("./example.txt", O_RDONLY);
 	get_next_line(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 03:56:54 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/05/13 06:14:48 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/05/13 21:54:40 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # include <unistd.h>
 # include <stdio.h>
 #include <fcntl.h>
+# include <stdlib.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+//extern int buffer_size = 42;
 
 #endif
